3957273faeb9b18b4cb8f01c27f2b05b79eb147d
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air.local>
2016-07-01 16:51:18 -0700

    linked styling sheets js jquery css added board and classes

--- js/app.js
+++ js/app.js
@@ -2,5 +2,15 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+var boardArray=['placeholder','.one_one','.one_two','.one_three','.two_one','.two_two','.two_three','.three_one','.three_two','.three_three'];
+
+$(.box).onClick('click', function(e){
+
+		console.log("yay");}
+}
+
+
+if()
+
 
 });
