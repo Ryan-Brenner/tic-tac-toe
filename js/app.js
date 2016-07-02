6fb36df111c079bc9efe75eed899fb2b7c4a62c1
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air.local>
2016-07-01 19:05:04 -0700

    modified borders to boxes; also began building functions for on click effect

--- js/app.js
+++ js/app.js
@@ -2,15 +2,24 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  window.onload= function load(){$('.box').css('background'="blue")};
+
+
 var boardArray=['placeholder','.one_one','.one_two','.one_three','.two_one','.two_two','.two_three','.three_one','.three_two','.three_three'];
 
-$(.box).onClick('click', function(e){
+$('.box').onClick('click', function(e){
 
-		console.log("yay");}
+	if(counter % 2 ===0  &&& $(this).color()===defaultColor){
+		$(this).setClass('playerOne')
+
+	}
+		else if( { $(this).addClass('playerTwo')}
+	
 }
 
 
-if()
+
+
 
 
 });
